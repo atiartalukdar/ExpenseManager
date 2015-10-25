d2b4569802dbdd1f908800eb3d619b878a312552
Atiar Talukdar <atiartalukdar@gmail.com>
2015-10-26 04:03:59 +0600

    add spinner & category to category list

--- app/src/main/java/expensemanager/com/nothing2says/talukdar/expensemanager/addExpense.java
+++ app/src/main/java/expensemanager/com/nothing2says/talukdar/expensemanager/addExpense.java
@@ -6,6 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +17,9 @@ import java.util.Calendar;
 
 public class addExpense extends AppCompatActivity {
     TextView tvMonth,tvFirstCurrentBalance;
+    Spinner ctgrySpinner;
+    private String[] categoryString;
+    private String selectedCategory="";
     protected final static int DEFAULT = 0;
     int temp = DEFAULT,currentMonthIncome = DEFAULT ;
     String[] month = {"January","February","March","April","May","June","Julay","August","September","October","November","December"};
@@ -20,8 +27,8 @@ public class addExpense extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
-        tvMonth = (TextView) findViewById(R.id.monthTV);
-        tvFirstCurrentBalance = (TextView) findViewById(R.id.currentBalanceTV);
+        initialize();
+        spinner();
         int yy,mm,dd;
         final Calendar c = Calendar.getInstance();
         //  yy = c.get(Calendar.YEAR);
@@ -44,9 +51,62 @@ public class addExpense extends AppCompatActivity {
             tvFirstCurrentBalance.setText("Current Balance "+temp+"Tk");
         }
 
+    }
 
+
+
+    //work with the spinners
+    public void spinner() {
+
+        ctgrySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+                                                     @Override
+                                                     public void onItemSelected(AdapterView<?> arg0, View view, int arg2, long arg3) {
+
+                                                         //indexExam = examSpinnerSE.getSelectedItemPosition();
+                                                         selectedCategory = arg0.getSelectedItem().toString();
+                                                         //Toast.makeText(ChalangeView.this, mSelectedItemExam+" is Selected", Toast.LENGTH_LONG).show();
+                                                     }
+
+                                                     @Override
+                                                     public void onNothingSelected(AdapterView<?> arg0) {
+                                                     }
+                                                 }
+        );
 
     }
+    public void initialize(){
+        //initialize view
+        ctgrySpinner = (Spinner)findViewById(R.id.categorySpinner);
+        tvMonth = (TextView) findViewById(R.id.monthTV);
+        tvFirstCurrentBalance = (TextView) findViewById(R.id.currentBalanceTV);
+
+        //initialize data source
+        categoryString = getResources().getStringArray(R.array.initialCategory);
+
+        // confirmButtonSE
+
+
+        //initialize view
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, categoryString);
+
+        //bind adapter and view
+        ctgrySpinner.setAdapter(adapter);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
