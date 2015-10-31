848cd43d939d8f77e8d863f74f188ff5f895cf5d
Atiar Talukdar <atiartalukdar@gmail.com>
2015-11-01 00:44:06 +0600

    add spinner & category to category list

--- app/src/main/java/expensemanager/com/nothing2says/talukdar/expensemanager/ManageIncome.java
+++ app/src/main/java/expensemanager/com/nothing2says/talukdar/expensemanager/ManageIncome.java
@@ -4,14 +4,61 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ManageIncome extends AppCompatActivity {
-
+    addExpense aE = new addExpense();
+    EditText edIncomeAmount,edIncomeNote;
+    int DEFAULT = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_income);
+
+        edIncomeAmount = (EditText) findViewById(R.id.incomeAmountED);
+        edIncomeNote = (EditText) findViewById(R.id.incomeAmountNoteED);
+
+
     }
+
+
+    public void subtractIncome(View view){
+        int previousBalance=DEFAULT,currentBanalce=DEFAULT,newBanalce=DEFAULT;
+        previousBalance = aE.readSharedPreference("myBalance","firstIncomeBanalce");
+        Toast.makeText(this,"Load Successfull "+previousBalance,Toast.LENGTH_LONG).show();
+    }
+
+    public  void addIncome(View view){
+
+    }
+
+    public void showAllIncome(View view){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
